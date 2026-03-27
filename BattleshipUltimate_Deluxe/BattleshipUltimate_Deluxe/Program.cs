aea91fb6474fa9cefbc7141953ee203e0fadeab7
Maximilien <40921999+Maxodie@users.noreply.github.com>
2026-03-27 13:00:44 +0100

    feat : initial commit

--- BattleshipUltimate_Deluxe/BattleshipUltimate_Deluxe/Program.cs
+++ BattleshipUltimate_Deluxe/BattleshipUltimate_Deluxe/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace MyApp
+{
+    internal class BattleshipUltimate_Deluxe
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
