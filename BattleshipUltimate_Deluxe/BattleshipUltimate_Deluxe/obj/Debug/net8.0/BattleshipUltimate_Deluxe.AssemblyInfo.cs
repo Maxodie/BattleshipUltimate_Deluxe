ff1ffc4793b827ef5132270153f4c08aea21dd8f
Maximilien <40921999+Maxodie@users.noreply.github.com>
2026-03-27 13:07:44 +0100

    feat : entrypoint

--- BattleshipUltimate_Deluxe/BattleshipUltimate_Deluxe/obj/Debug/net8.0/BattleshipUltimate_Deluxe.AssemblyInfo.cs
+++ BattleshipUltimate_Deluxe/BattleshipUltimate_Deluxe/obj/Debug/net8.0/BattleshipUltimate_Deluxe.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BattleshipUltimate_Deluxe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aea91fb6474fa9cefbc7141953ee203e0fadeab7")]
 [assembly: System.Reflection.AssemblyProductAttribute("BattleshipUltimate_Deluxe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BattleshipUltimate_Deluxe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
